318e4834647392a58fbf4cacd744a10e9502528b
Romanillo <Romanillo504@gmail.com>
2014-06-08 18:12:51 +0100

    tests

--- public/test/tests.js
+++ public/test/tests.js
@@ -4,7 +4,8 @@ suite( 'Parser', function(){
 
   test('Titulo: ', function(){
     var result = FilmInfo.parse("TITULO Prueba;.");
-    assert.deepEqual(result, "'<!DOCTYPE html> <br> <html>         <br> <head>          <br> <title> FilmInfo </title> <br> <meta http-equiv='Content-type' content='text/html' ;='' charset='UTF-8'> <br> <style> <br> body {background-color:'#FFC77D';} <br> h1 {text-align:center;} <br> </style> </head> <br> <body> <br>   <h1>Prueba</h1> <br> </body> <br> </html> '");
+	var esperado = '<!DOCTYPE html> <br> <html>         <br> <head>          <br> <title> FilmInfo </title> <br> <meta http-equiv='Content-type' content='text/html' ;='' charset='UTF-8'> <br> <style> <br> body {background-color:'#FFC77D';} <br> h1 {text-align:center;} <br> </style> </head> <br> <body> <br>   <h1>Prueba</h1> <br> </body> <br> </html> ';
+    assert.deepEqual(result, esperado);
   });
 
 });
