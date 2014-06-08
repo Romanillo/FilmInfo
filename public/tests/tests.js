03f4e50eccfc25305e5f464248f44ea7e440d277
Romanillo <Romanillo504@gmail.com>
2014-06-08 18:50:57 +0100

    errores

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -1,13 +1,19 @@
 var assert = chai.assert; 
 
 suite( 'Parser', function(){
-
+alert("Suite!");
   test('Titulo: ', function(){
+  alert("test!");
 	var entrada = 'TITULO Prueba;.';
+	alert("entrada!");
     var result = FilmInfo.parse(entrada);
+	alert("result!");
 	var esperado = '<!DOCTYPE html> <br> <html>         <br> <head>          <br> <title> FilmInfo </title> <br> <meta http-equiv="Content-type" content="text/html" ;="" charset="UTF-8"> <br> <style> <br> body {background-color:"#FFC77D";} <br> h1 {text-align:center;} <br> </style> </head> <br> <body> <br>   <h1>Prueba</h1> <br> </body> <br> </html> ';
-    assert.deepEqual(JSON.stringify(result,undefined,2), esperado);
+    alert("esperado!");
+	assert.deepEqual(JSON.stringify(result,undefined,2), esperado);
+	alert("json!");
   });
+  alert("test!");
 
 });
 
