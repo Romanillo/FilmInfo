1b75e553f8a9b1d5426792629bc61b5312e9999e
Romanillo <Romanillo504@gmail.com>
2014-06-08 19:01:18 +0100

    errores

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -4,11 +4,11 @@ suite( 'Parser', function(){
 alert("Suite!");
   test('Titulo: ', function(){
   alert("test!");
-	var entrada = 'TITULO Prueba;.';
+	var entrada = "TITULO Prueba;.";
 	alert("entrada!");
     var result = FilmInfo.parse(entrada);
 	alert("result!");
-	var esperado = '<!DOCTYPE html> <br> <html>         <br> <head>          <br> <title> FilmInfo </title> <br> <meta http-equiv="Content-type" content="text/html" ;="" charset="UTF-8"> <br> <style> <br> body {background-color:"#FFC77D";} <br> h1 {text-align:center;} <br> </style> </head> <br> <body> <br>   <h1>Prueba</h1> <br> </body> <br> </html> ';
+	var esperado = "<!DOCTYPE html> <br> <html>         <br> <head>          <br> <title> FilmInfo </title> <br> <meta http-equiv='Content-type' content='text/html' ;='' charset='UTF-8'> <br> <style> <br> body {background-color:'#FFC77D';} <br> h1 {text-align:center;} <br> </style> </head> <br> <body> <br>   <h1>Prueba</h1> <br> </body> <br> </html> ";
     alert("esperado!");
 	assert.deepEqual(JSON.stringify(result,undefined,2), esperado);
 	alert("json!");
@@ -16,5 +16,3 @@ alert("Suite!");
   alert("test!");
 
 });
-
-alert("Acaban los test!");
