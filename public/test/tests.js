3558d7f8a5a0f9cf850a05eee7ceb928fb5a6e01
Romanillo <Romanillo504@gmail.com>
2014-06-08 18:04:40 +0100

    tests

--- public/test/tests.js
+++ public/test/tests.js
@@ -1,5 +1,10 @@
 var assert = chai.assert; 
 
-suite( 'FilmInfo', function(){
+suite( 'Parser', function(){
+
+  test('Titulo: ', function(){
+    var result = FilmInfo.parse("TITULO Prueba;.");
+    assert.deepEqual(result, "'<!DOCTYPE html> <br> <html>         <br> <head>          <br> <title> FilmInfo </title> <br> <meta http-equiv='Content-type' content='text/html' ;='' charset='UTF-8'> <br> <style> <br> body {background-color:'#FFC77D';} <br> h1 {text-align:center;} <br> </style> </head> <br> <body> <br>   <h1>Prueba</h1> <br> </body> <br> </html> '");
+  });
 
 });
